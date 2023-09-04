197d1ff779ac6c7bec387585cf976f293e20ac88
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-04 19:55:07 -0300

    feat: Criar rotas para home e usuário

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes as ReactRoutes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
+import User from './pages/user/User'
 
 const Routes = () => {
   return (
     <ReactRoutes>
         <Route path='/' Component={Home}/>
+        <Route path='/user/:userName' Component={User}/>
     </ReactRoutes>
   )
 }
