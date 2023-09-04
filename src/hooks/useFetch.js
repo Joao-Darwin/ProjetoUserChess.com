357ff67c4dee89df2c73b2cd4c3d48ff33e5d8ca
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-04 19:58:28 -0300

    feat: Definir função para buscar usuários por username

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,15 @@
+const urlAPiChess = "https://api.chess.com"
+
+export const findUserByName = async (userName) => {
+    try {
+        let url = `${urlAPiChess}/pub/player/${userName}`
+
+        const response = await fetch(url);
+        const codeResponse = response.status;
+        const data = await response.json();
+
+        return { data, codeResponse };
+    } catch (error) {
+        console.log(error.message);
+    }
+}
