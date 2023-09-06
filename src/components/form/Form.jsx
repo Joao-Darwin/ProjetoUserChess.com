8196203f105483bcc91555b141101dad33d79ec8
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-05 22:45:43 -0300

    feat: Aplicar estilização ao formulário

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import iconeSearch from "../../assets/iconSearch.png"
+
+import "./Form.css"
+
 const Form = () => {
-  const nagivate = useNavigate();
+  const navigate = useNavigate();
 
   const [userName, setUserName] = useState('');
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    nagivate(`/user/${userName}`);
+    if (userName !== '') {
+      navigate(`/user/${userName}`);
+    } else {
+      alert("Você precisa informar o username");
+    }
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='User Name' onChange={(e) => setUserName(e.target.value)} />
-        <input type="submit" value="Find" />
-      </form>
+    <div className='form'>
+      <input autoFocus className='inputUserName' type="text" placeholder='User Name' onChange={(e) => setUserName(e.target.value)} />
+      <img className='inputButtonSearch' src={iconeSearch} alt="search--v1" onClick={handleSubmit} />
     </div>
   )
 }
