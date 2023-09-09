d5cd7702864a553f69cedb53dd639a5fbf87c6fb
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-09 00:53:50 -0300

    fix: Corrigir do botão de ir para a twitch

--- src/pages/users/User.jsx
+++ src/pages/users/User.jsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { findUserByName } from '../../hooks/useFetch';
 
-import "./User.css"
-// import UserStats from '../../components/userStats/UserStats';
+import UserStats from '../../components/userStats/UserStats';
+import "./User.css";
 
 const User = () => {
 
   const { userName } = useParams();
-
-  const [loading, setLoading] = useState(true);
 
   const [codeResponse, setCodeResponse] = useState();
   const [avatar, setAvatar] = useState();
@@ -23,10 +21,6 @@ const User = () => {
   useEffect(() => {
 
     findUserByNameAsync(userName);
-
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
   }, [userName])
 
   async function findUserByNameAsync(userName) {
@@ -42,7 +36,7 @@ const User = () => {
     setPlan(data.status);
     setUrlUser(data.url);
 
-    if (isStreamer) {
+    if (data.twitch_url) {
       setUrlTwitch(data.twitch_url);
     }
 
@@ -50,36 +44,33 @@ const User = () => {
   }
 
   return (
-    <div>
-      {loading ? (
-        <h1>Carregando...</h1>
-      ) : (
-        <>
-          {codeResponse !== 200 ? (
-            <h3>User {userName} não encontrado</h3>
-          ) : (
-            <>
-              {avatar && <img className='avatar' src={avatar} alt='Avatar no chess.com' />}
-              <div>
-                <h3>{userName}</h3>
-                {league && <p>{league}</p>}<br />
-              </div>
-              <div className='divInfoUser'>
-                {followers && <p><strong>{followers}</strong><br />Followers</p>}
-                {isStreamer && <a href={urlTwitch} target='_blank'><img width="48" height="48" src="https://img.icons8.com/color/48/twitch--v1.png" alt="twitch--v1" /></a>}
-                {plan && <p><strong>{plan.toUpperCase()}</strong><br />Plan</p>}
-              </div>
-              {/* <UserStats userName={userName}/> */}
-              {urlUser && (
-                <a title='See on Site' href={urlUser} target="_blank" rel="noopener noreferrer" className='linkSeeOnSite'>
-                  <img width="48" height="48" src="https://img.icons8.com/color/48/info--v1.png" alt="info--v1" />
-                </a>
-              )}
-            </>
-          )}
-        </>
-      )}
-    </div>
+    codeResponse ? (
+      <div>
+        {codeResponse !== 200 ? (
+          <h3>User {userName} não encontrado</h3>
+        ) : (
+          <>
+            {avatar && <img className='avatar' src={avatar} alt='Avatar no chess.com' />}
+            <div>
+              <h3>{userName}</h3>
+              {league && <p>{league}</p>}<br />
+            </div>
+            <div className='divInfoUser'>
+              {followers && <p><strong>{followers}</strong><br />Followers</p>}
+              {isStreamer && <a href={urlTwitch} target='_blank'><img width="48" height="48" src="https://img.icons8.com/color/48/twitch--v1.png" alt="twitch--v1" /></a>}
+              {plan && <p><strong>{plan.toUpperCase()}</strong><br />Plan</p>}
+            </div>
+            <UserStats userName={userName} />
+            {urlUser && (
+              <a title='See on Site' href={urlUser} target="_blank" rel="noopener noreferrer" className='linkSeeOnSite'>
+                <img width="48" height="48" src="https://img.icons8.com/color/48/info--v1.png" alt="info--v1" />
+              </a>
+            )}
+          </>
+        )}
+      </div>) : (
+      <h3>Carregando...</h3>
+    )
   );
 }
 
