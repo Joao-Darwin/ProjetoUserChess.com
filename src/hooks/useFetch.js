db424f8bb27bbd3a97a546c05fb1419c1079d9d9
João Darwin <joaodarwin.ip22@gmail.com>
2023-09-09 00:53:13 -0300

    feat: Criar função para requisitar dados das estatísticas

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -13,3 +13,17 @@ export const findUserByName = async (userName) => {
         console.log(error.message);
     }
 }
+
+export const findStatsByUser = async (userName) => {
+    try {
+        let url = `${urlAPiChess}/pub/player/${userName}/stats`;
+
+        const response = await fetch(url);
+        const codeResponse = response.status;
+        const data = await response.json();
+
+        return { data, codeResponse }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
